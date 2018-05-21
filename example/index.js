454a01945fc49d71eab53d2938f95445cffd4bfd
Conor Hastings <hastings.conorm@gmail.com>
2018-05-20 21:38:42 -0700

    add example

--- example/index.js
+++ example/index.js
@@ -1,1 +1,13 @@
-localize('appName');
+import { Text } from 'react-localize';
+class X extends React.Component {
+    render() {
+        return (
+            <div>
+                <span>
+                    {localize("appName")}
+                </span>
+                <Text message="prop.Val" values={["y"]} style={{ color: 'blue' }} />
+            </div>
+        );
+    }
+}
